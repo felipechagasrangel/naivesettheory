e58cc20f3b339c66692503ddff7f6be5fa62baa4
francisco.rangel <francisco.rangel@BRSPO-WS-0038>
2021-04-04 09:09:42 -0300

    feat: updating with the real killing of the thread

--- main.py
+++ main.py
@@ -9,38 +9,50 @@ def rabbit_connection(config, event_queue):
     while True:
         if event_queue.qsize() > 0:
             event = event_queue.get()
-            if event == 'test':
-                print('Test event: {event}'.format(event=event))
-            elif event == 'end':
-                print('End event: {event}'.format(event=event))
+            if event['msg_type'] == 'test':
+                print('Thread: {name}, Test event: {event}'.format(name= th.get_ident(),event=event))
+            if event['msg_type'] == 'end':
+                print('Thread: {name}, End event: {event}'.format(name=th.get_ident(), event=event))
+                break
+    print('Thread {} killed'.format(th.get_ident()))
 
 # Thread de controle que recebe os eventos
-def controller_thread(main_context_queue, event_queue):
+def controller_thread(main_context_queue, rabbit_threads):
     while True:
         if main_context_queue.qsize() > 0:
             msg = main_context_queue.get()
-            event_queue.put(msg['msg_type'])
+            if msg['msg_type'] == 'test':
+                rabbit_threads[msg['msg']]['event_queue'].put(msg)
+            elif msg['msg_type'] == 'create':
+                pass
+            elif msg['msg_type'] == 'end':
+                print('Matar a thread: {thread}'.format(thread=msg['msg']))
+                rabbit_threads[msg['msg']]['event_queue'].put(msg)
         else:
             time.sleep(1)
 
 # Processo que controla o rabbit. Uma thread recebe eventos e as outras fazem a conexão com o Rabbit
 def rabbit_main(main_context_queue, rabbit_config):
-    event_queue = mp.Queue()
-    controller = th.Thread(target=controller_thread, args=(main_context_queue, event_queue,))
 
     print('Threads started')
 
     rabbit_threads = {}
     for config in rabbit_config:
-        rabbit_threads[config['exchange']] = th.Thread(target=rabbit_connection, args=(config, event_queue,))
+        exchange_name = config['exchange']
+        rabbit_threads[exchange_name] = {}
+        event_queue = mp.Queue()
+        rabbit_threads[exchange_name]['thread'] = th.Thread(target=rabbit_connection, args=(config, event_queue,), name=exchange_name)
+        rabbit_threads[exchange_name]['event_queue'] = event_queue
+
+    controller = th.Thread(target=controller_thread, args=(main_context_queue, rabbit_threads,))
 
     controller.start()
     for config in rabbit_config:
-        rabbit_threads[config['exchange']].start()
+        rabbit_threads[config['exchange']]['thread'].start()
 
     controller.join()
     for config in rabbit_config:
-        rabbit_threads[config['exchange']].join()
+        rabbit_threads[config['exchange']]['thread'].join()
 
     print('Threads ended')
 
@@ -49,11 +61,11 @@ def rabbit_main(main_context_queue, rabbit_config):
 def main(main_context_queue):
 
     time.sleep(2)
-    msg = {'timestamp': time.time(), 'msg_type': 'test', 'msg': None}
+    msg = {'timestamp': time.time(), 'msg_type': 'test', 'msg': 'exchange1'}
     main_context_queue.put(msg)
 
     time.sleep(2)
-    msg = {'timestamp': time.time(), 'msg_type': 'end', 'msg': None}
+    msg = {'timestamp': time.time(), 'msg_type': 'end', 'msg': 'exchange2'}
     main_context_queue.put(msg)
 
 

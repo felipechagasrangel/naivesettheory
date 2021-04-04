1dd502179abdceda940c629f17843a15efb2b6ee
francisco.rangel <francisco.rangel@BRSPO-WS-0038>
2021-04-04 14:48:52 -0300

    fix: corrected the creation/destroying of threads

--- main.py
+++ main.py
@@ -4,51 +4,67 @@ import pika
 import threading as th
 import multiprocessing as mp
 
+
 # Thread que fica bloqueada recebendo mensagens do Rabbit
-def rabbit_connection(config, event_queue):
-    while True:
+def rabbit_connection(config):
+    event_queue = config['event_queue']
+    print('Thread {} started'.format(th.get_ident()))
+
+    while config['keep_running']:
         if event_queue.qsize() > 0:
             event = event_queue.get()
             if event['msg_type'] == 'test':
-                print('Thread: {name}, Test event: {event}'.format(name= th.get_ident(),event=event))
+                print('Thread: {name}, Test event: {event}'.format(name=th.get_ident(), event=event))
             if event['msg_type'] == 'end':
                 print('Thread: {name}, End event: {event}'.format(name=th.get_ident(), event=event))
                 break
     print('Thread {} killed'.format(th.get_ident()))
 
+
 # Thread de controle que recebe os eventos
 def controller_thread(main_context_queue, rabbit_threads):
     while True:
+
+        for exchange in rabbit_threads:
+            if (rabbit_threads[exchange]['keep_running'] is True and (rabbit_threads[exchange]['thread'] is None)):
+                rabbit_threads[exchange]['thread'] = th.Thread(target=rabbit_connection, args=(rabbit_threads[exchange],), name=exchange)
+                rabbit_threads[exchange]['thread'].start()
+
         if main_context_queue.qsize() > 0:
             msg = main_context_queue.get()
             if msg['msg_type'] == 'test':
                 rabbit_threads[msg['msg']]['event_queue'].put(msg)
             elif msg['msg_type'] == 'create':
-                pass
+                rabbit_threads[msg['msg']]['keep_running'] = True
             elif msg['msg_type'] == 'end':
                 print('Matar a thread: {thread}'.format(thread=msg['msg']))
-                rabbit_threads[msg['msg']]['event_queue'].put(msg)
+                rabbit_threads[msg['msg']]['keep_running'] = False
+                rabbit_threads[msg['msg']]['thread'] = None
         else:
             time.sleep(1)
 
+
 # Processo que controla o rabbit. Uma thread recebe eventos e as outras fazem a conexão com o Rabbit
 def rabbit_main(main_context_queue, rabbit_config):
-
     print('Threads started')
 
+    keep_running = True
     rabbit_threads = {}
     for config in rabbit_config:
         exchange_name = config['exchange']
         rabbit_threads[exchange_name] = {}
         event_queue = mp.Queue()
-        rabbit_threads[exchange_name]['thread'] = th.Thread(target=rabbit_connection, args=(config, event_queue,), name=exchange_name)
+        rabbit_threads[exchange_name]['keep_running'] = True
+        rabbit_threads[exchange_name]['thread'] = None
         rabbit_threads[exchange_name]['event_queue'] = event_queue
 
     controller = th.Thread(target=controller_thread, args=(main_context_queue, rabbit_threads,))
 
     controller.start()
-    for config in rabbit_config:
-        rabbit_threads[config['exchange']]['thread'].start()
+
+
+    while keep_running:
+        time.sleep(1);
 
     controller.join()
     for config in rabbit_config:
@@ -57,9 +73,8 @@ def rabbit_main(main_context_queue, rabbit_config):
     print('Threads ended')
 
 
-#Processo principal que envia mensagens na fila principal para os outros processos pegarem
+# Processo principal que envia mensagens na fila principal para os outros processos pegarem
 def main(main_context_queue):
-
     time.sleep(2)
     msg = {'timestamp': time.time(), 'msg_type': 'test', 'msg': 'exchange1'}
     main_context_queue.put(msg)
@@ -70,7 +85,7 @@ def main(main_context_queue):
 
 
 if __name__ == '__main__':
-#   Cria um processo principal e um processo que controla apenas o Rabbit
+    #   Cria um processo principal e um processo que controla apenas o Rabbit
     rabbit_config = [
         {'host': None, 'exchange': 'exchange1', 'queue': None},
         {'host': None, 'exchange': 'exchange2', 'queue': None},
@@ -79,9 +94,9 @@ if __name__ == '__main__':
 
     main_context = mp.Process(target=main, args=(main_context_queue,))
     rabbit_process = mp.Process(target=rabbit_main, args=(main_context_queue, rabbit_config,))
-    
+
     main_context.start()
     rabbit_process.start()
-    
+
     main_context.join()
     rabbit_process.join()
